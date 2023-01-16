413b9f91910cb23655ba22b38c0a1b71b04dd953
Prashant Raj <109718738+luc1f3r-r@users.noreply.github.com>
2023-01-16 20:51:56 +0530

    Update bash script
    The script above is designed to append the contents of one CSV file to another existing file and remove duplicate values, and also it checks if the appending file doesn't exist it should return a message.
    
    The script starts by defining the names of the two files, existing_file.csv and append_file.csv. The existing_file.csv is the file that will be appended with the contents of the append_file.csv.
    
    Next, the script uses an if statement to check if the file append_file.csv exists. This is done by using the -f option with the test command, which tests whether the specified file exists and is a regular file. If the file exists, the script will use the cat command to append the contents of the CSV file, append_file.csv, to the existing file, existing_file.csv.
    Then it sorts the contents of the existing file using the sort command and removes the duplicate lines using the -u option and output the result to the same existing file using the -o option.
    
    If the file doesn't exist, the script will return a message "File append_file not found"

--- bash_script.sh
+++ bash_script.sh
@@ -4,8 +4,23 @@
 
 /usr/bin/python3 /Users/prashantraj/Documents/cron/script.py
 
-#/usr/local/bin/aws cp rhisac* s3://rhisac-intel-feed
+sleep 180
+
+existing_file="rhisac.csv"
+append_file="rhisac_iocs_last*.csv"
+
+#Check if the appending file exists
+if [ -f "$append_file" ]; then
+   # Append contents and remove duplicates
+   cat $append_file >> $existing_file
+   sort -u $existing_file -o $existing_file
+   echo "Appended contents of $append_file to $existing_file and removed duplicates"
+else
+   echo "File $append_file not found"
+fi
+
+/usr/local/bin/aws cp /home/jenkins/threat-feed/rhisac.csv s3://rhisac-intel-feed/rhisac.csv
 
 sleep 900
 
-#mv rhisac* /home/jenkins/RHISAC
+mv /home/jenkins/rhisac_iocs_last*.csv /home/jenkins/RHISAC
