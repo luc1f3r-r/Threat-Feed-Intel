1c931628f5b443e0d3128e9eeaa0734d9828ed0a
Prashant Raj <109718738+luc1f3r-r@users.noreply.github.com>
2023-01-23 12:26:33 +0530

    Update bash_script.sh

--- bash_script.sh
+++ bash_script.sh
@@ -2,25 +2,37 @@
 
 #0 0 * * * /bin/bash bash_script.sh
 
-/usr/bin/python3 /Users/prashantraj/Documents/cron/script.py
+echo "Python script executing"
+/usr/bin/python3 /home/jenkins/threat_feed/script.py
 
-sleep 180
+sleep 30
 
-existing_file="rhisac.csv"
-append_file="rhisac_iocs_last*.csv"
+mv /home/jenkins/rhisac_iocs_last* /home/jenkins/threat_feed
 
-#Check if the appending file exists
-if [ -f "$append_file" ]; then
-   # Append contents and remove duplicates
-   cat $append_file >> $existing_file
-   sort -u $existing_file -o $existing_file
-   echo "Appended contents of $append_file to $existing_file and removed duplicates"
-else
-   echo "File $append_file not found"
-fi
+echo "Appending the new file"
+cat /home/jenkins/threat_feed/rhisac_iocs_last* >> /home/jenkins/threat_feed/new_rhisac.csv
 
-/usr/local/bin/aws cp /home/jenkins/threat-feed/rhisac.csv s3://rhisac-intel-feed/rhisac.csv
+sleep 20
 
-sleep 900
+echo "Removing Duplicates"
+awk '!seen[$0]++ || NR==1' /home/jenkins/threat_feed/new_rhisac.csv > /home/jenkins/threat_feed/rhisac.csv
+#sort -t, -k1,1 -u /home/jenkins/threat_feed/rhisac.csv > /home/jenkins/threat_feed/rhisac.csv
 
-mv /home/jenkins/rhisac_iocs_last*.csv /home/jenkins/RHISAC
+#echo "Adding the new column"
+#sed -i '1i ioc_value,ioc_indicatorType,ioc_firstSeen,ioc_lastSeen,ioc_value_list' /home/jenkins/threat_feed/rhisac.csv
+
+
+sleep 20
+
+echo "Uploading the updated file to S3 Bucket"
+/usr/local/bin/aws s3 cp /home/jenkins/threat_feed/rhisac.csv s3://rhisac-intel-feed/
+
+sleep 10
+
+echo "Removing additional files"
+rm /home/jenkins/threat_feed/new_rhisac.csv
+
+echo "Cleaning up the Folder"
+mv /home/jenkins/threat_feed/rhisac_iocs_last* /home/jenkins/RHISAC
+
+echo "Task completed!"
