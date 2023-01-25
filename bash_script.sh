38529a2fa417d87ea9d4903d2399a7684435e354
Prashant Raj <109718738+luc1f3r-r@users.noreply.github.com>
2023-01-25 12:53:05 +0530

    Update bash script
    Gave a more detailed explanation of the script above for better understanding

--- bash_script.sh
+++ bash_script.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-#0 0 * * * /bin/bash bash_script.sh
+# This script is executed by cron daemon on a linux machine. It's scheduled to run at 0 0 * * * (every day at 00:00)
 
 echo "Python script executing"
+# This line runs a python script located at "/home/jenkins/threat_feed/script.py" using python3 interpreter.
 /usr/bin/python3 /home/jenkins/threat_feed/script.py
 
+# Wait for 30 seconds
 sleep 30
 
+# Move files with names matching "rhisac_iocs_last*" from the current directory to "/home/jenkins/threat_feed"
 mv /home/jenkins/rhisac_iocs_last* /home/jenkins/threat_feed
 
 echo "Appending the new file"
+# Append the contents of all files with names matching "rhisac_iocs_last*" in the "/home/jenkins/threat_feed" directory to "/home/jenkins/threat_feed/new_rhisac.csv"
 cat /home/jenkins/threat_feed/rhisac_iocs_last* >> /home/jenkins/threat_feed/new_rhisac.csv
 
+# Wait for 20 seconds
 sleep 20
 
 echo "Removing Duplicates"
+# Remove duplicate lines from "/home/jenkins/threat_feed/new_rhisac.csv" and save the result to "/home/jenkins/threat_feed/rhisac.csv"
 awk '!seen[$0]++ || NR==1' /home/jenkins/threat_feed/new_rhisac.csv > /home/jenkins/threat_feed/rhisac.csv
 #sort -t, -k1,1 -u /home/jenkins/threat_feed/rhisac.csv > /home/jenkins/threat_feed/rhisac.csv
 
@@ -22,17 +28,22 @@ awk '!seen[$0]++ || NR==1' /home/jenkins/threat_feed/new_rhisac.csv > /home/jenk
 #sed -i '1i ioc_value,ioc_indicatorType,ioc_firstSeen,ioc_lastSeen,ioc_value_list' /home/jenkins/threat_feed/rhisac.csv
 
 
+# Wait for 20 seconds
 sleep 20
 
 echo "Uploading the updated file to S3 Bucket"
+# Upload the file "/home/jenkins/threat_feed/rhisac.csv" to an S3 bucket with the name "rhisac-intel-feed"
 /usr/local/bin/aws s3 cp /home/jenkins/threat_feed/rhisac.csv s3://rhisac-intel-feed/
 
+# Wait for 10 seconds
 sleep 10
 
 echo "Removing additional files"
+# Remove the file "/home/jenkins/threat_feed/new_rhisac.csv"
 rm /home/jenkins/threat_feed/new_rhisac.csv
 
 echo "Cleaning up the Folder"
+# Move files with names matching "rhisac_iocs_last*" from "/home/jenkins/threat_feed" to "/home/jenkins/RHISAC"
 mv /home/jenkins/threat_feed/rhisac_iocs_last* /home/jenkins/RHISAC
 
 echo "Task completed!"
